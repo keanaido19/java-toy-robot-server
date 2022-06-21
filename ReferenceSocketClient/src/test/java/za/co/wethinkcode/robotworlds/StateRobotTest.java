a167320e74e63aa38496d4a25340e9e55eb59d44
keanaido021@ <keanaido021@student.wethinkcode.co.za>
2022-06-21 19:33:13 +0200

    cleaned up state test

--- ReferenceSocketClient/src/test/java/za/co/wethinkcode/robotworlds/StateRobotTest.java
+++ ReferenceSocketClient/src/test/java/za/co/wethinkcode/robotworlds/StateRobotTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 

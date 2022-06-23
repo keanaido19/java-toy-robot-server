d3aea1a2828043f8b6931b60943fd0b219c53001
Keaton Naidoo <keanaido021@student.wethinkcode.co.za>
2022-06-23 22:50:00 +0000

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,15 +1,25 @@
 !#/bin/bash
 
-kill -9 $(lsof -t -i:5000)
 mvn -B clean install -DskipTests
+
+kill -9 $(lsof -t -i:5000)
+
 mvn exec:java &
 mvn test -Dtest="world1x1.AcceptanceTests*" -pl ReferenceSocketClient
+
 kill -9 $(lsof -t -i:5000)
+
 mvn exec:java -Dexec.args="-s 2" &
 mvn test -Dtest="world2x2.AcceptanceTests*" -pl ReferenceSocketClient
+
+kill -9 $(lsof -t -i:5000)
+
 mvn exec:java -Dexec.args="-s 2 -o 1,1" &
 mvn test -Dtest="obstacle1_1.AcceptanceTests*" -pl ReferenceSocketClient
+
 kill -9 $(lsof -t -i:5000)
+
 mvn exec:java -Dexec.args="-s 2 -o 0,1"  &
 mvn test -Dtest="obstacle0_1.AcceptanceTests*" -pl ReferenceSocketClient
+
 kill -9 $(lsof -t -i:5000)

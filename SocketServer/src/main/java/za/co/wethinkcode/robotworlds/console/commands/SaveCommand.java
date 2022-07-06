4ba256ab5a5a0c609af30b57bcf52c630be47e1f
keanaido021 <keanaido021@student.wethinkcode.co.za>
2022-07-06 12:46:35 +0200

    Completed save command

--- SocketServer/src/main/java/za/co/wethinkcode/robotworlds/console/commands/SaveCommand.java
+++ SocketServer/src/main/java/za/co/wethinkcode/robotworlds/console/commands/SaveCommand.java
@@ -1,6 +1,12 @@
 package za.co.wethinkcode.robotworlds.console.commands;
 
 import za.co.wethinkcode.robotworlds.RobotServer;
+import za.co.wethinkcode.robotworlds.databaseconnectors.DbConnector;
+import za.co.wethinkcode.robotworlds.databaseconnectors.SQLiteDbConnector;
+import za.co.wethinkcode.robotworlds.dbobjects.WorldDbObject;
+import za.co.wethinkcode.robotworlds.world.WorldDbObjectConverter;
+
+import java.sql.SQLException;
 
 public class SaveCommand extends ServerCommand {
     private final String worldName;
@@ -11,6 +17,22 @@ public class SaveCommand extends ServerCommand {
 
     @Override
     public boolean execute(RobotServer server) {
+        WorldDbObject worldDbObject =
+                WorldDbObjectConverter.getWorldDbObject(server.getWorld());
+        try {
+            DbConnector databaseConnector =
+                    new SQLiteDbConnector("world.sqlite");
+            databaseConnector.saveWorld(worldName, worldDbObject);
+
+            System.out.printf(
+                    "Saved world as \"%s\" in database.%n",
+                    worldName
+            );
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         return true;
     }
 }

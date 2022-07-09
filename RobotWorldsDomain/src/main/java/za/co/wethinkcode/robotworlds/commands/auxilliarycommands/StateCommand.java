3789ccf76eb6374344f0ab69035b00a0c6b4ea25
keanaido021@ <keanaido021@student.wethinkcode.co.za>
2022-07-10 00:43:15 +0200

    Completed StateCommand

--- RobotWorldsDomain/src/main/java/za/co/wethinkcode/robotworlds/commands/auxilliarycommands/StateCommand.java
+++ RobotWorldsDomain/src/main/java/za/co/wethinkcode/robotworlds/commands/auxilliarycommands/StateCommand.java
@@ -1,6 +1,11 @@
 package za.co.wethinkcode.robotworlds.commands.auxilliarycommands;
 
+import za.co.wethinkcode.robotworlds.Play;
+import za.co.wethinkcode.robotworlds.commands.CommandResult;
 import za.co.wethinkcode.robotworlds.response.JsonResponse;
+import za.co.wethinkcode.robotworlds.world.World;
+import za.co.wethinkcode.robotworlds.world.builders.DataMapBuilder;
+import za.co.wethinkcode.robotworlds.world.objects.robots.Robot;
 
 public class StateCommand extends AuxiliaryCommand {
 
@@ -10,6 +15,13 @@ public class StateCommand extends AuxiliaryCommand {
 
     @Override
     public JsonResponse execute() {
-        return null;
+        World world = Play.getWorld();
+        Robot robot = world.getRobot(robotName);
+        return
+                new JsonResponse(
+                        CommandResult.OK,
+                        DataMapBuilder.getDataMap(world, robot),
+                        robot.getRobotData()
+                );
     }
 }

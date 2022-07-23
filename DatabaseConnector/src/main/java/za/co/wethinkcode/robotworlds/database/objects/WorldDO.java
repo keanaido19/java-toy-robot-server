502e9e21b1126c73a6e8caf211f61e6d7f1d675e
keanaido021@ <keanaido021@student.wethinkcode.co.za>
2022-07-23 21:39:56 +0200

    It works, yay!

--- DatabaseConnector/src/main/java/za/co/wethinkcode/robotworlds/database/objects/WorldDO.java
+++ DatabaseConnector/src/main/java/za/co/wethinkcode/robotworlds/database/objects/WorldDO.java
@@ -1,12 +1,9 @@
 package za.co.wethinkcode.robotworlds.database.objects;
 
-import net.lemnik.eodsql.ResultColumn;
-
 import java.util.List;
 import java.util.Objects;
 
 public class WorldDO {
-    @ResultColumn(value = "worldName")
     private final String worldName;
     private final WorldDataDO worldData;
     private final List<WorldObjectDO> obstacles, pits, mines;

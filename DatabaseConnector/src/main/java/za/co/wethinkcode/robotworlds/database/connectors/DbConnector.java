438398e7dea17eadbb136c09171e6a1a7adcc27f
Keaton Naidoo <keanaido021@student.wethinkcode.co.za>
2022-07-23 17:59:11 +0000

    Revert "Resolve "Resolve eodsql dependency""
    This reverts commit 3263c773e244f9f76a9f587dd9a80b7f4a2db510

--- DatabaseConnector/src/main/java/za/co/wethinkcode/robotworlds/database/connectors/DbConnector.java
+++ DatabaseConnector/src/main/java/za/co/wethinkcode/robotworlds/database/connectors/DbConnector.java
@@ -1,9 +1,9 @@
 package za.co.wethinkcode.robotworlds.database.connectors;
 
-
 import za.co.wethinkcode.robotworlds.database.objects.WorldDO;
 
 import java.sql.SQLException;
+
 public interface DbConnector {
     void saveWorld(String worldName, WorldDO world) throws SQLException;
 
